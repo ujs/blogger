6dbff27cbbb877b80d744eab8f2c7a464896f459
ujala shanker <ujalashanker@gmail.com>
2015-03-23 10:59:42 -0700

    delete/edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+ include ArticlesHelper
  def index
  	@articles = Article.all
  end
@@ -12,10 +13,20 @@ class ArticlesController < ApplicationController
  end
 
  def create
- 	@article = Article.new
- 	@article.title = params[:article][:title]
+ 	@article = Article.new(article_params)
+
  	@article.save
  	redirect_to article_path(@article)
+ end
+
+ def edit
+  @article = Article.find(params[:id])
+end
+
+ def destroy
+ 	@article = Article.find(params[:id])
+ 	@article.destroy
+ 	redirect_to :ArticlesController => "index", :action => 'index'
  end
 
 end
